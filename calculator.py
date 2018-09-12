5ebeb41a28a5984c0b13c5be0c8bcc70aab179f7
yang7824 <yanghui-002@qq.com>
2018-09-13 02:41:38 +0800

    save

--- calculator.py
+++ calculator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import csv
 
 class Args(object):
 
@@ -48,66 +49,86 @@ class UserData(object):
 
 class lncomeTaxCalculator(object):
     def __init__(self):
-        pass
-   
-    def calc_for_all_userdata(self):
         self.args = Args()
         self.config = Config(self.args.test_path)
         self.user = UserData(self.args.user_path)
-        result = ""
-        for user_id, wages in self.user.userdata.items():
-            she_bao = wages*(self.config.get_config('YangLao') \
+
+    def _user_she_bao(self, wages):
+        she_bao = wages*(self.config.get_config('YangLao') \
                     + self.config.get_config('YiLiao')  \
                     + self.config.get_config('YangLao') \
                     + self.config.get_config('ShiYe')  \
                     + self.config.get_config('GongShang') \
                     + self.config.get_config('ShengYu') \
                     + self.config.get_config('GongJiJin'))
-            print("11111", she_bao)    
-    
-            tax_wages = wages - she_bao - 3500
+        return she_bao
+
+    def user_tax(self, wages, she_bao):
+        tax_wages = wages - she_bao - 3500
+        tax_x = 0
+        tax_rate = 0.00
+        if tax_wages > 0 and tax_wages <= 1500:
+            tax_rate = 0.03
             tax_x = 0
-            tax_rate = 0.00
-            if tax_wages > 0 and tax_wages <= 1500:
-                tax_rate = 0.03
-                tax_x = 0
-            elif tax_wages > 1500 and tax_wages <= 4500:
-                tax_rate = 0.10
-                tax_x = 105
-            elif tax_wages > 4500 and tax_wages <= 9000:
-                tax_rate = 0.20
-                tax_x = 555
-            elif tax_wages > 9000 and tax_wages <= 35000:
-                tax_rate = 0.25
-                tax_x = 1005
-            elif tax_wages > 35000 and tax_wages <= 55000:
-                tax_rate = 0.30
-                tax_x = 275
-            elif tax_wages > 55000 and tax_wages <= 80000:
-                tax_rate = 0.35
-                tax_x = 5505
-            elif tax_wages > 80000:
-                tax_rate = 0.45
-                tax_x = 13505
+        elif tax_wages > 1500 and tax_wages <= 4500:
+            tax_rate = 0.10
+            tax_x = 105
+        elif tax_wages > 4500 and tax_wages <= 9000:
+            tax_rate = 0.20
+            tax_x = 555
+        elif tax_wages > 9000 and tax_wages <= 35000:
+            tax_rate = 0.25
+            tax_x = 1005
+        elif tax_wages > 35000 and tax_wages <= 55000:
+            tax_rate = 0.30
+            tax_x = 275
+        elif tax_wages > 55000 and tax_wages <= 80000:
+            tax_rate = 0.35
+            tax_x = 5505
+        elif tax_wages > 80000:
+            tax_rate = 0.45
+            tax_x = 13505
 
-            if tax_wages > 0:    
-                tax = tax_wages * tax_rate - tax_x
-            else:
-                tax = 0.00 
+        if tax_wages > 0:
+            tax = tax_wages * tax_rate - tax_x
+        else:
+            tax = 0.00
+
+        return tax
+
+    def finally_wages(self, wages, she_bao, tax):
+        f_wages = wages - she_bao - tax
+        return f_wages
+
+
+    def calc_for_all_userdata(self):
+#        self.args = Args()
+#        self.config = Config(self.args.test_path)
+#        self.user = UserData(self.args.user_path)
+        result = []
+        for user_id, wages in self.user.userdata.items():
+            she_bao = self._user_she_bao(wages) 
+            tax = self.user_tax(wages, she_bao)            
+            f_wages = self.finally_wages(wages, she_bao, tax)
             
-            f_wages = wages - she_bao - tax
+            result.append([str(user_id), str(wages), format(she_bao, '.2f'), \
+                      format(tax, '.2f'), format(f_wages, '.2f')])
 
-            result += str(user_id) + ',' + str(wages) + ',' + format(she_bao, '.2f') \
-                 + ',' + format(tax, '.2f') + ',' + format(f_wages, '.2f') + '\n'
-
-        print(result)
+#        print(result)
         return result
 
+    def export(self, default = 'csv'):
+        result = self.calc_for_all_userdata()
+        with open(self.args.gongzi_path, 'w') as f:
+            writer = csv.writer(f)
+            writer.writerows(result)
+            print(result)
 
 if __name__ == '__main__':
 
     test = lncomeTaxCalculator()
-    print(test.calc_for_all_userdata())
+    test.export()
+#    print(test.calc_for_all_userdata())
 
 
 
