63aeba26381e6bf533d2d131d6208108cfa423a6
yang7824 <yanghui-002@qq.com>
2018-09-12 23:14:41 +0800

    seva file2

--- calculator.py
+++ calculator.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python3
 import sys
+
+class Args(object):
+
+    def __init__(self):
+        try:
+            self.args = sys.argv[1:]
+            self.test_path = self.args[self.args.index('-c') + 1]
+            self.user_path = self.args[self.args.index('-d') + 1]
+            self.gongzi_path = self.args[self.args.index('-o') + 1]
+            self.user_path2 = 'test'
+        except:
+            print("input Errer!")
+            sys.exit()
+
+class Config(object):
+    def __init__(self, path):
+        self.config = self._read_config(path)
+
+    def _read_config(self, path):
+        config = {}
+        with open(path, 'rt') as config_file:
+            for line in config_file:
+                key_vaule = line.split('=')
+#                key_vaule[0].strip('\n')
+#                key_vaule[1].strip('\n')
+                key_vaule[0] = key_vaule[0].strip()
+                key_vaule[1] = key_vaule[1].strip()
+                config[key_vaule[0]] = key_vaule[1]
+        return config
+
+    def get_config(self, key):
+        return self.config[key] 
+
 def fax_count(wages):
     try:
         wages = int(wages)
@@ -22,7 +55,7 @@ def fax_count(wages):
         tax_x = 1005
     elif tax_wages > 35000 and tax_wages <= 55000:
         tax_rate = 0.30
-        tax_x = 2755
+        tax_x = 275
     elif tax_wages > 55000 and tax_wages <= 80000:
         tax_rate = 0.35
         tax_x = 5505
@@ -43,14 +76,19 @@ def wages_final(wages):
     f_wages = wages - wages*(0.08 + 0.02 + 0.005 + 0.06) - fax_count(wages)
     return f_wages
 
+args = Args()
+config = Config(args.test_path)
 
-all_employee_dict = sys.argv[1:]
-for employee in all_employee_dict:
-    employee_dict = employee.split(':')
-    employee_id = employee_dict[0]
-    employee_wages = employee_dict[1]
+print(config.get_config('JiShuL'))
+print(args.user_path)
+print(str(config.config))
+#all_employee_dict = sys.argv[1:]
+#for employee in all_employee_dict:
+#    employee_dict = employee.split(':')
+#    employee_id = employee_dict[0]
+#    employee_wages = employee_dict[1]
 
 #    print(employee_id,':',wages_final(employee_wages))
 #    print("{}:{}".format(employee_id,format(wages_final(employee_wages),".2f")))
 
-    print("{}:{:.2f}".format(employee_id,wages_final(employee_wages)))
+#    print("{}:{:.2f}".format(employee_id,wages_final(employee_wages)))
