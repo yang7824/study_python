d099a78d71c759441a327b1cf26367a3dfa2c8c2
yang7824 <yanghui-002@qq.com>
2018-09-13 01:35:41 +0800

    save

--- calculator.py
+++ calculator.py
@@ -31,57 +31,97 @@ class Config(object):
         return config
 
     def get_config(self, key):
-        return self.config[key] 
+        return float(self.config[key]) 
 
-def fax_count(wages):
-    try:
-        wages = int(wages)
-    except:
-        print("Parameter Error")
-    tax_wages = wages - wages*(0.08 + 0.02 + 0.005 + 0.06)- 3500
-    tax_x = 0
-    tax_rate = 0.00
-    if tax_wages > 0 and tax_wages <= 1500:
-        tax_rate = 0.03
-        tax_x = 0
-    elif tax_wages > 1500 and tax_wages <= 4500:
-        tax_rate = 0.10
-        tax_x = 105
-    elif tax_wages > 4500 and tax_wages <= 9000:
-        tax_rate = 0.20
-        tax_x = 555
-    elif tax_wages > 9000 and tax_wages <= 35000:
-        tax_rate = 0.25
-        tax_x = 1005
-    elif tax_wages > 35000 and tax_wages <= 55000:
-        tax_rate = 0.30
-        tax_x = 275
-    elif tax_wages > 55000 and tax_wages <= 80000:
-        tax_rate = 0.35
-        tax_x = 5505
-    elif tax_wages > 80000:
-        tax_rate = 0.45
-        tax_x = 13505
-    if tax_wages > 0:    
-        tax = tax_wages * tax_rate - tax_x
-    else:
-        tax = 0.00 
-    return tax
+class UserData(object):
+    def __init__(self, path):
+        self.userdata = self._read_users_data(path)
 
-def wages_final(wages):
-    try:
-        wages = int(wages)
-    except:
-        print("Parameter Error")
-    f_wages = wages - wages*(0.08 + 0.02 + 0.005 + 0.06) - fax_count(wages)
-    return f_wages
+    def _read_users_data(self, path):
+        _userdata = {}
+        with open(path) as user_file:
+            for line in user_file:
+                id_wages = line.split(',')
+                _userdata[int(id_wages[0])] = int(id_wages[1])
+        return _userdata
 
-args = Args()
-config = Config(args.test_path)
 
-print(config.get_config('JiShuL'))
-print(args.user_path)
-print(str(config.config))
+class lncomeTaxCalculator(object):
+    def __init__(self):
+        pass
+   
+    def calc_for_all_userdata(self):
+        self.args = Args()
+        self.config = Config(self.args.test_path)
+        self.user = UserData(self.args.user_path)
+        result = ""
+        for user_id, wages in self.user.userdata.items():
+            she_bao = wages*(self.config.get_config('YangLao') \
+                    + self.config.get_config('YiLiao')  \
+                    + self.config.get_config('YangLao') \
+                    + self.config.get_config('ShiYe')  \
+                    + self.config.get_config('GongShang') \
+                    + self.config.get_config('ShengYu') \
+                    + self.config.get_config('GongJiJin'))
+            print("11111", she_bao)    
+    
+            tax_wages = wages - she_bao - 3500
+            tax_x = 0
+            tax_rate = 0.00
+            if tax_wages > 0 and tax_wages <= 1500:
+                tax_rate = 0.03
+                tax_x = 0
+            elif tax_wages > 1500 and tax_wages <= 4500:
+                tax_rate = 0.10
+                tax_x = 105
+            elif tax_wages > 4500 and tax_wages <= 9000:
+                tax_rate = 0.20
+                tax_x = 555
+            elif tax_wages > 9000 and tax_wages <= 35000:
+                tax_rate = 0.25
+                tax_x = 1005
+            elif tax_wages > 35000 and tax_wages <= 55000:
+                tax_rate = 0.30
+                tax_x = 275
+            elif tax_wages > 55000 and tax_wages <= 80000:
+                tax_rate = 0.35
+                tax_x = 5505
+            elif tax_wages > 80000:
+                tax_rate = 0.45
+                tax_x = 13505
+
+            if tax_wages > 0:    
+                tax = tax_wages * tax_rate - tax_x
+            else:
+                tax = 0.00 
+            
+            f_wages = wages - she_bao - tax
+
+            result += str(user_id) + ',' + str(wages) + ',' + format(she_bao, '.2f') \
+                 + ',' + format(tax, '.2f') + ',' + format(f_wages, '.2f') + '\n'
+
+        print(result)
+        return result
+
+
+if __name__ == '__main__':
+
+    test = lncomeTaxCalculator()
+    print(test.calc_for_all_userdata())
+
+
+
+
+
+
+#args = Args()
+#config = Config(args.test_path)
+#user = UserData(args.user_path)
+
+#print(config.get_config('JiShuL'))
+#print(args.user_path)
+#print(str(config.config))
+#print(str(user.userdata))
 #all_employee_dict = sys.argv[1:]
 #for employee in all_employee_dict:
 #    employee_dict = employee.split(':')
